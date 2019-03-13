ae514349895f3d5488bca64cccaed1a29cafc2c5
Murad Akhundov <muradakhdev@gmail.com>
2019-03-13 18:42:51 -0400

    cadded new endpoints and changed old ones

--- src/main/kotlin/dev/murad/acorn/AcornController.kt
+++ src/main/kotlin/dev/murad/acorn/AcornController.kt
@@ -2,16 +2,15 @@ package dev.murad.acorn
 
 import auth.Acorn
 import entity.enrol.EnrolledCourse
-import exception.LoginFailedException
+import entity.plan.PlannedCourse
 import org.springframework.web.bind.annotation.*
-import java.lang.NullPointerException
 import java.lang.RuntimeException
 
 @RestController
 class AcornController {
 
-    @PostMapping("/auth")
-    fun auth(account: Account): MutableList<EnrolledCourse>? {
+    @PostMapping("/enrolled")
+    fun enrolled(account: Account): MutableList<EnrolledCourse>? {
         val acorn = Acorn(account.utorid, account.password)
 
         try {
@@ -22,5 +21,46 @@ class AcornController {
 
         return (acorn.courseManager.appliedCourses)
     }
+
+    @PostMapping("/planned")
+    fun planned(account: Account): MutableList<PlannedCourse>? {
+        val acorn = Acorn(account.utorid, account.password)
+
+        try {
+            acorn.doLogin()
+        } catch (e: RuntimeException) {
+            throw AuthException("Authentication Error")
+        }
+
+        return (acorn.courseManager.plannedCourses)
+    }
+
+    @PostMapping("/transcript")
+    fun transcript(account: Account): String? {
+        val acorn = Acorn(account.utorid, account.password)
+
+        try {
+            acorn.doLogin()
+        } catch (e: RuntimeException) {
+            throw AuthException("Authentication Error")
+        }
+
+        return (acorn.gradeManager.gradeHtml)
+    }
+
+    @PostMapping("/registrations")
+    fun registrations(account: Account): MutableList<String>? {
+        val acorn = Acorn(account.utorid, account.password)
+
+        try {
+            acorn.doLogin()
+        } catch (e: RuntimeException) {
+            throw AuthException("Authentication Error")
+        }
+
+        return (acorn.registrationManager.eligibleRegistrations)
+    }
+
+
 
 }
